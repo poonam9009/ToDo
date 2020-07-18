95fc8c1b3bc073b95c528eaa49509113fd440d19
poonam9009 <ppoonia381@gmail.com>
2020-07-18 13:37:37 +0530

    Updates

---
+++ precache-manifest.bee8d4944cfea47d2a09527d984743c2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b4a6a95697edeee25d5284a332c55c0",
+    "url": "/poonam9009/ToDo.git/index.html"
+  },
+  {
+    "revision": "af4545a6e422efd0abf4",
+    "url": "/poonam9009/ToDo.git/static/css/main.fd01f0ec.chunk.css"
+  },
+  {
+    "revision": "a1723d93cfadc3a83b28",
+    "url": "/poonam9009/ToDo.git/static/js/2.ccbbd2f3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/poonam9009/ToDo.git/static/js/2.ccbbd2f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af4545a6e422efd0abf4",
+    "url": "/poonam9009/ToDo.git/static/js/main.c8e7eebc.chunk.js"
+  },
+  {
+    "revision": "56df17c25cc526cdc583",
+    "url": "/poonam9009/ToDo.git/static/js/runtime-main.6c4b82e1.js"
+  }
+]);
